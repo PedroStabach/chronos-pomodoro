c6f6645de8e717ff30cc2cfdb90d9a1b802612e1
PedroStabach <pedroaugustostabach3@gmail.com>
2025-06-09 15:53:16 -0300

    menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,16 +3,16 @@ import { HistoryIcon, HomeIcon, SettingsIcon, SunIcon } from 'lucide-react';
 export function Menu() {
     return (
         <div className={styles.Menu}>
-            <a className={styles.MenuLink} href="#">
+            <a className={styles.MenuLink} href="#" aria-label='Menu'>
                 <HomeIcon />
             </a>
-            <a className={styles.MenuLink} href="#">
+            <a className={styles.MenuLink} href="#" aria-label='historico'>
                 <HistoryIcon />
             </a>
-            <a className={styles.MenuLink} href="#">
+            <a className={styles.MenuLink} href="#"aria-label='configuracoes'>
                 <SettingsIcon />
             </a>
-            <a className={styles.MenuLink} href="#">
+            <a className={styles.MenuLink} href="#" aria-label='tema'>
                 <SunIcon />
             </a>
         </div>
