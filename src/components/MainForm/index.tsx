0ace5fcbb213dfe1922d5cb13099dc72b75a3168
PedroStabach <pedroaugustostabach3@gmail.com>
2025-08-26 19:36:04 -0300

    mainform

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,13 +2,30 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
+import { useRef, useState } from "react";
 
 
 export function MainForm() {
+    const [taskName, setTaskName] = useState();
+    const taskNameInput = useRef<HTMLInputElement>(null);
+    const numero = useRef(0);
+    function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+        numero.current += 1;
+        console.log('Criando nova tarefa ', taskName);
+    }
     return (
-        <form className='form' action=''>
+        <form onSubmit={handleCreateNewTask} className='form' action=''>
                     <div className='formRow'>
-                       <DefaultInput id='meuInput' type='text' labelText='task' placeholder='Digite aqui' />
+                       <DefaultInput 
+                       id='meuInput' 
+                       type='text' 
+                       labelText='task' 
+                       placeholder='Digite aqui' 
+                       value={taskName} 
+                       onChange={e => setTaskName(e.target.value)}
+                       ref={taskNameInput}
+                        />
                     </div>
 
                     <div className='formRow'>
